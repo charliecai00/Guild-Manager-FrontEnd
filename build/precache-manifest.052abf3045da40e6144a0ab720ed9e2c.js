efdb6a89f620da755849ab200f7c11044116c54b
Charlie Cai <lc4181@nyu.edu>
2023-02-18 22:24:18 -0500

    deploy to heroku

---
+++ build/precache-manifest.052abf3045da40e6144a0ab720ed9e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc99b4683882c234b33e",
+    "url": "/static/css/main.fc66c609.chunk.css"
+  },
+  {
+    "revision": "cc99b4683882c234b33e",
+    "url": "/static/js/main.30b25d93.chunk.js"
+  },
+  {
+    "revision": "44379f9d8a742a556af9",
+    "url": "/static/js/runtime~main.1b973b83.js"
+  },
+  {
+    "revision": "3b1149610b73f3131802",
+    "url": "/static/js/2.968e7eeb.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "1b8ad299671cd8249945f4a71e98b19e",
+    "url": "/index.html"
+  }
+];
