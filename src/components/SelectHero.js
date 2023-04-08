1db7ef0acc77ac039dd9c618da4b268678ca785d
Zhiquan Cao <zc1548@nyu.edu>
2023-04-07 21:54:47 -0400

    add Hero is working

--- src/components/SelectHero.js
+++ src/components/SelectHero.js
@@ -1,5 +1,4 @@
 import "./pop_up_window.css";
-// import './shared.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { ExitIcon } from "./ExitIcon";
@@ -7,31 +6,33 @@ import { HERO_NOT_IN_PARTY_URL, ADD_HERO_TO_PARTY_URL } from "./url";
 
 export const SelectHero = (props) => {
     const [heroes, setHeroes] = useState([]);
-    console.log("party id: "+props.party_id);
+    console.log("party id: " + props.party_id);
+    const getHeroes = () => {
+        axios.post(HERO_NOT_IN_PARTY_URL, { "id": parseInt(props.party_id) })
+            .then((response) => {
+                setHeroes(response.data.Response);
+            })
+    }
     useEffect(() => {
         if (props.party_id === undefined || props.party_id === null) {
             return;
         }
-        axios.post(HERO_NOT_IN_PARTY_URL, {"id": parseInt(props.party_id)})
-            .then((response) => {
-                console.log(response.data);
-                setHeroes(response.data.Response);
-            })
+        getHeroes();
     }, [props.party_id]);
 
     const addHero = (event) => {
         const heroId = event.target.value;
-        
-        // axios.post(ADD_HERO_TO_PARTY_URL, { "id": heroId, "guild_id": guildDetail.ID})
-        //   .then((response) => {
-        //     console.log(response.data.Response);
-        //     if (response.data.Response !== "Success"){
-        //       alert(response.data.Response);
-        //     };
-        //   })
-        //   .catch((error) => {
-        //     console.log(error);
-        //   });
+        axios.post(ADD_HERO_TO_PARTY_URL, { "id": heroId, "party_id": props.party_id })
+            .then((response) => {
+                console.log(response.data.Response);
+                if (response.data.Response !== "Success") {
+                    alert(response.data.Response);
+                };
+                getHeroes();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     return (
---
+++ src/components/SelectQuest.js
@@ -0,0 +1,60 @@
+import "./pop_up_window.css";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ExitIcon } from "./ExitIcon";
+import { Quest_NOT_IN_PARTY_URL, ADD_Quest_TO_PARTY_URL } from "./url";
+
+export const SelectQuest = (props) => {
+    const [Questes, setQuestes] = useState([]);
+    console.log("party id: "+props.party_id);
+    useEffect(() => {
+        if (props.party_id === undefined || props.party_id === null) {
+            return;
+        }
+        axios.post(Quest_NOT_IN_PARTY_URL, {"id": parseInt(props.party_id)})
+            .then((response) => {
+                console.log(response.data);
+                setQuestes(response.data.Response);
+            })
+    }, [props.party_id]);
+
+    const addQuest = (event) => {
+        const QuestId = event.target.value;
+        
+        // axios.post(ADD_Quest_TO_PARTY_URL, { "id": QuestId, "guild_id": guildDetail.ID})
+        //   .then((response) => {
+        //     console.log(response.data.Response);
+        //     if (response.data.Response !== "Success"){
+        //       alert(response.data.Response);
+        //     };
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
+    }
+
+    return (
+        <div className="pop_up_box" style={props.style}>
+            <ExitIcon value="/guild" />
+            <div className="pop_up_title">
+                <div style={{ position: "absolute", left: "38%", top: "20%" }}>
+                    Select Quest
+                </div>
+            </div>
+            <div className="list">
+                {
+                    Questes.map((Quest, ind) => {
+                        return (
+                            <button className="select_Quest_scrollable"
+                                key={ind}
+                                onClick={addQuest}
+                                value={Quest.ID}>
+                                {Quest.Name}
+                            </button>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
